1fe4960c15423f02f0a462b218d6845920eec3fb
PrabuDhakUCSD <pdhakshi@eng.ucsd.edu>
2016-02-24 18:07:03 -0800

    Comments.

--- 2015/CountNumbersWithEvenOddDiffOfOne.java
+++ 2015/CountNumbersWithEvenOddDiffOfOne.java
@@ -1,10 +1,15 @@
 import java.util.Arrays;
 
+/*
+ * Find #of n digit numbers which have sum(evenDigits) - sum(oddDigits) = 1
+ */ 
 public class CountNumbersWithEvenOddDiffOfOne {
 
     public static int countNumbers(int n) {
+        // dp[n][d] = #of n digit numbers with even odd diff of d.
         int[][] dp = new int[n][10];
         
+        // For 1 digit numbers, each diff in [0...9] has exactly one number.
         Arrays.fill(dp[0], 1);
         
         for (int i=1; i<n; i++) {
@@ -23,6 +28,12 @@ public class CountNumbersWithEvenOddDiffOfOne {
         if (dp[numDigits-1][diff] == -1) {
             int count = 0;
             for (int firstDigitValue=0; firstDigitValue<=9; firstDigitValue++) {
+                // Required diff = d (let's say 1)
+                // Current even lead = firstDigitValue (let's say 9)
+                // Required even lead l = d-firstDigitValue (1 - 9 = -8)
+                // even-odd = l ==> odd-even = -l (i.e., 8)
+                // next recursive call will start with odd as the first digit
+                // and hence our -ve logic will just work fine.
                 count += helper(dp, numDigits-1, firstDigitValue-diff);
             }
             
